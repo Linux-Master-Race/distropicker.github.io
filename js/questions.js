2a880c5a44515c038178b9cdd8ceb99564769199
whitephoenix0 <spencerburris.sb@gmail.com>
2016-07-18 09:10:23 -0700

    Rework js to use arrays

--- js/questions.js
+++ js/questions.js
@@ -1,71 +1,98 @@
-function setq1() {
-	setQuestion("1. What is your *NIX operating system skill level?");
-	setAns(1, "Beginner", "You have never used a BSD or GNU/Linux operating system before, or have used one very little.");
-	setAns(2, "Intermediate", "You have used a BSD or GNU/Linux operating system before, and have a fair amount of relevant technical knowledge.");
-	setAns(3, "Expert", "You are quite knowledable about the inner workings of a BSD or GNU/Linux system.");
-	setInfo("N/A");
-}
+qtitle.push("1. What is your *NIX operating system skill level?");
+qtitle1.push("Beginner");
+qans1.push("You have never used a BSD or GNU/Linux operating system before, or have used one very little.");
+qtitle2.push("Intermediate");
+qans2.push("Intermediate", "You have used a BSD or GNU/Linux operating system before, and have a fair amount of relevant technical knowledge.");
+qtitle3.push("Expert");
+qans3.push("You are quite knowledable about the inner workings of a BSD or GNU/Linux system.");
+qinfo.push("N/A");
 
-function setq2() {
-	setQuestion("2. How do you prefer your software?");
-	setAns(1, "Stable", "Software is rock-solid and nearly never crashes, but can be fairly outdated.");
-	setAns(2, "Inbetween", "A mix between being updated and stable, low number of crashes while still getting new features and fixes.");
-	setAns(3, "Up-to-date", "Software is hot off the presses and can have new features and improvements, but has unstable tendencies.");
-	setInfo("If you rely on Linux for a job, you should go for stable software.");
-}
+qtitle.push("2. How do you prefer your software?");
+qtitle1.push("Stable");
+qans1.push("Software is rock-solid and nearly never crashes, but can be fairly outdated.");
+qtitle2.push("Inbetween");
+qans2.push("A mix between being updated and stable, low number of crashes while still getting new features and fixes.");
+qtitle3.push("Up-to-date");
+qans3.push("Software is hot off the presses and can have new features and improvements, but has unstable tendencies.");
+qinfo.push("If you rely on Linux for a job, you should go for stable software.");
 
-function setq3() {
- 	setQuestion("3. How much preinstalled software do you want?");
-	setAns(1, "Minimal", "Included just the bare essentials, I will install my own software.");
-	setAns(2, "Average", "Includes software for common computing tasks such as web browsing, document editing, and music/video playback.");
-	setAns(3, "Specialized", "Includes specialized software for creating virtual machines, programming, video editing, 3d modeling, etc, by default.");
-	setInfo("N/A");
-}
+qtitle.push("3. How much preinstalled software do you want?");
+qtitle1.push("Minimal");
+qans1.push("Included just the bare essentials, I will install my own software.");
+qtitle2.push("Average");
+qans2.push("Includes software for common computing tasks such as web browsing, document editing, and music/video playback.");
+qtitle3.push("Specialized");
+qans3.push("Includes specialized software for creating virtual machines, programming, video editing, 3d modeling, etc, by default.");
+qinfo.push("N/A");
 
-function setq4() {
-	setQuestion("4. Do you prefer GNU/Linux or BSD?");
-	setAns(1, "GNU/Linux", "GNU/Linux is often more supported and is better for beginners, as it is more popular.");
-	setAns(2, "Either", "You have no preference between GNU/Linux and a BSD distribution.");
-	setAns(3, "BSD", "BSD is a better choice if your concern is security or running on old or exotic hardware.");
-	setInfo("You may want to research the differences, or just choose \"either\"");
-}
+qtitle.push("4. Do you prefer GNU/Linux or BSD?");
+qtitle1.push("GNU/Linux");
+qans1.push("GNU/Linux is often more supported and is better for beginners, as it is more popular.");
+qtitle2.push("Either");
+qans2.push("You have no preference between GNU/Linux and a BSD distribution.");
+qtitle3.push("BSD");
+qans3.push("BSD is a better choice if your concern is security or running on old or exotic hardware.");
+qinfo.push("You may want to research the differences, or just choose \"either\"");
 
-function setq5() {
-	setQuestion("5. Do you want systemd in your distro?");
-	setAns(1, "Yes", "You want systemd to be installed. Systemd generally has better support than its alternatives.");
-	setAns(2, "Either", "You are undecided, don't care, or don't know what systemd is.");
-	setAns(3, "No", "You want to avoid systemd in favor of an alternative such as runit or openrc. Choose this if you previously answered \"BSD\".");
-	setInfo("To learn about why some want to avoid systemd; visit without-systemd.org");
-}
+qtitle.push("5. Do you want systemd in your distro?");
+qtitle1.push("Yes");
+qans1.push("Yes", "You want systemd to be installed. Systemd generally has better support than its alternatives.");
+qtitle2.push("Either");
+qans2.push("You are undecided, don't care, or don't know what systemd is.");
+qtitle3.push("No");
+qans3.push("You want to avoid systemd in favor of an alternative such as runit or openrc. Choose this if you previously answered \"BSD\".");
+qinfo.push("To learn about why some want to avoid systemd; visit without-systemd.org");
 
-function setq6() {
-	setQuestion("6. Do you want an operating system that is light on resources?");
-	setAns(1, "No", "You don't need to focus on finding a lightweight operating system.");
-	setAns(2, "Maybe", "Your PC is not that fast or a little old, you want something lightweight but not to make sacrifices for it.");
-	setAns(3, "Yes", "You have an old or slow computer, or just want something as light and fast as possible.");
-	setInfo("This question mainly determines what desktop environment will be best.");
-}
+qtitle.push("6. Do you want an operating system that is light on resources?");
+qtitle1.push("Yes");
+qans1.push("You have an old or slow computer, or just want something as light and fast as possible.");
+qtitle2.push("Maybe");
+qans2.push("Your PC is not that fast or a little old, you want something lightweight but not to make sacrifices for it.");
+qtitle3.push("No");
+qans3.push("You don't need to focus on finding a lightweight operating system.");
+qinfo.push("This question mainly determines what desktop environment will be best.");
 
-function setq7() {
-	setQuestion("7. Do you want your distro to come with a graphical user interface installed?");
-	setAns(1, "Yes", "I want a GUI installed by default. Beginners should choose this.");
-	setAns(2, "Maybe", "I have no preference, either way works for me.");
-	setAns(3, "No", "I want to install and configure my own.");
-	setInfo("N/A");
-}	
+qtitle.push("7. Do you want your distro to come with a graphical user interface installed?");
+qtitle1.push("Yes");
+qans1.push("I want a GUI installed by default. Beginners should choose this.");
+qtitle2.push("Maybe");
+qans2.push("Maybe", "I have no preference, either way works for me.");
+qtitle3.push("No");
+qans3.push("I want to install and configure my own.");
+qinfo.push("N/A");
 
-function setq8() {
-	setQuestion("8. Do you want all of your software to be free?");
-	setAns(1, "Yes", "I want to avoid every bit of proprietary software");
-	setAns(2, "Maybe", "I prefer to stick to free software, but some proprietary software is alright with me.");
-	setAns(3, "No", "I hate freedom, let all the corporations steal my privacy and strip away my rights.");
-	setInfo("By \"free\" we mean as in freedom, not as in price; learn more at fsf.org");
-}
+qtitle.push("8. Do you want all of your software to be free?");
+qtitle1.push("Yes");
+qans1.push("I want to avoid every bit of proprietary software");
+qtitle2.push("Maybe");
+qans2.push("I prefer to stick to free software, but some proprietary software is alright with me.");
+qtitle3.push("No");
+qans3.push("I hate freedom, let all the corporations steal my privacy and strip away my rights.");
+qinfo.push("By \"free\" we mean as in freedom, not as in price; learn more at fsf.org");
 
-function setq9() {
-	setQuestion("9. Do you mind compiling software from source code?");
-	setAns(1, "Yes", "I don't want to have to compile anything, binaries only please. Beginners should choose this.");
-	setAns(2, "Maybe", "Compiling is alright with me, but I don't want to have to compile everything.");
-	setAns(3, "No", "I prefer to compile things, I'd prefer to compile most software on my computer.");
-	setInfo("Compiling is the process of turning source code into \"binaries\" your computer can run.");
-}
+qtitle.push("9. Do you mind compiling software from source code?");
+qtitle1.push("Yes");
+qans1.push("I don't want to have to compile anything, binaries only please. Beginners should choose this.");
+qtitle2.push("Maybe");
+qans2.push("Compiling is alright with me, but I don't want to have to compile everything.");
+qtitle3.push("No");
+qans3.push("I prefer to compile things, I'd prefer to compile most software on my computer.");
+qinfo.push("Compiling is the process of turning source code into \"binaries\" your computer can run.");
+
+qtitle.push("10. Do you prefer command line or graphical user interfaces?");
+qtitle1.push("Command-Line");
+qans1.push("You prefer programs to be command line based, accessed from the terminal.");
+qtitle2.push("Either");
+qans2.push("You are happy with command line or graphical programs.");
+qtitle3.push("Graphical");
+qans3.push("You prefer programs to have graphical user interfaces (GUIs). Recommended for beginners.");
+qinfo.push("For more info on CLI vs GUI: computerhope.com/issues/ch000619.htm");
+
+qtitle.push("11. Do you want your distro to have an installer or do you want to install it manually?");
+qtitle1.push("Installer");
+qans1.push("You want an installer to walk you through installation and configuration. Recommended for beginners.");
+qtitle2.push("Either");
+qans2.push("You are fine with manual installation or installers.");
+qtitle3.push("Manual");
+qans3.push("I want to install and configure my distro myself.");
+qinfo.push("N/A")
--- js/quiz.js
+++ js/quiz.js
@@ -1,60 +1,57 @@
 var answers = [];
+var qtitle = [];
+var qtitle1 = [],
+    qans1 = [];
+var qtitle2 = [],
+    qans2 = [];
+var qtitle3 = [],
+    qans3 = [];
+var qinfo = [];
+var currentNumber = 1;
 
-function setQuestion(newText) {
-	document.getElementById("question").innerHTML=newText;
-}
+function setQuizQuestion(questionNumber) {
+    // Set main question title
+    document.getElementById("question").innerHTML = qtitle[questionNumber];
 
-function setAns(number, title, desc) {
-	document.getElementById("ans" + number.toString() + "_title").innerHTML=title;
-	document.getElementById("ans" + number.toString() + "_desc").innerHTML=desc;	
-}
+    // Set answer titles and descriptions
+    document.getElementById("ans1" + "_title").innerHTML = qtitle1[questionNumber];
+    document.getElementById("ans1" + "_desc").innerHTML = qans1[questionNumber];
+    document.getElementById("ans2" + "_title").innerHTML = qtitle2[questionNumber];
+    document.getElementById("ans2" + "_desc").innerHTML = qans2[questionNumber];
+    document.getElementById("ans3" + "_title").innerHTML = qtitle3[questionNumber];
+    document.getElementById("ans3" + "_desc").innerHTML = qans3[questionNumber];
 
-function setInfo(newText) {
-	document.getElementById("info_text").innerHTML=newText;
+    // Set question info
+    document.getElementById("info_text").innerHTML = qinfo[questionNumber];
 }
 
 function saveAnswer(elementId) {
-	answers.push(document.getElementById(elementId).textContent.toLowerCase());
+    answers.push(document.getElementById(elementId).textContent.toLowerCase());
 }
 
 function nextQuestion() {
-	currentNumber = Number(document.getElementById("question").textContent[0]);
-	console.log(currentNumber);	
-	
-	if (currentNumber == 1)
-		setq2();
-	else if (currentNumber == 2)
-		setq3();
-	else if (currentNumber == 3)
-		setq4();
-	else if (currentNumber == 4)
-		setq5();
-	else if (currentNumber == 5)
-		setq6();
-	else if (currentNumber == 6)
-		setq7();
-	else if (currentNumber == 7)
-		setq8();
-	else if (currentNumber == 8)
-		setq9();
+    if (currentNumber < 11) {
+        setQuizQuestion(currentNumber);
+        currentNumber = currentNumber + 1;
+        console.log(answers);
+    }
 }
 
-function setupHandlers() {
-	document.getElementById("ans1").addEventListener("click", function() {
-		saveAnswer("ans1_title");
-		nextQuestion();
-	}, false);
+document.getElementById("ans1").addEventListener("click", function() {
+    saveAnswer("ans1_title");
+    nextQuestion();
+}, false);
 
-	document.getElementById("ans2").addEventListener("click", function() {
-		saveAnswer("ans2_title");
-		nextQuestion();
-	}, false);
+document.getElementById("ans2").addEventListener("click", function() {
+    saveAnswer("ans2_title");
+    nextQuestion();
+}, false);
 
-	document.getElementById("ans3").addEventListener("click", function() {
-		saveAnswer("ans3_title");
-		nextQuestion();
-	}, false);
+document.getElementById("ans3").addEventListener("click", function() {
+    saveAnswer("ans3_title");
+    nextQuestion();
+}, false);
+
+window.onload = function() {
+  setQuizQuestion(0);
 }
-
-setupHandlers();
-setq1();
